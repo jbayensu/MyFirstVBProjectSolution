866d3bfe5d9c26b87a3e76be05a3f0d61700e503
John Bob Ayensu <johnbayensu@gmail.com>
2024-12-25 07:12:57 +0000

    added a tesing message

--- MyFirstVBProject/Form1.vb
+++ MyFirstVBProject/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub btn_ShowMsg_Click(sender As Object, e As EventArgs) Handles btn_ShowMsg.Click
-        MsgBox("Hello World, nothing is working")
+        MsgBox("Hello World, testing from other end")
     End Sub
 End Class
